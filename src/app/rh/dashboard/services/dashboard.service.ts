894cf4d9cc40e0eb8dbc641f44ff6748105e99a7
Michel Alves <ewave.malves@tcp.com.br>
2024-12-19 22:21:51 -0300

    mudado api

--- src/app/rh/dashboard/services/dashboard.service.ts
+++ src/app/rh/dashboard/services/dashboard.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -13,20 +13,25 @@ export class DashboardService {
 
   // Método genérico para construir o corpo da requisição
   private buildRequestBody(
+
     codCusto: Array<any>,
     codDir: Array<any>,
     codArea: Array<any>,
     codDep: Array<any>,
     codFunc: Array<any>,
     dataIni?: string | Date
+
+
   ): any {
     return {
+
       codCusto: codCusto || '',
       codDir: codDir || '',
       codArea: codArea || '',
       codDep: codDep || '',
       codFunc: codFunc || '',
       dataIni: dataIni || ''
+      
     };
   }
 
@@ -40,42 +45,42 @@ export class DashboardService {
   // POST para custos
   postCustos(codCusto: Array<any>, codDir: Array<any>, codArea: Array<any>, codDep: Array<any>, codFunc: Array<any>): Observable<any> {
     const url = `${this.apiUrl}/get_custo`;
-    const body = this.buildRequestBody(codCusto, codDir, codArea, codDep, codFunc);
+    const body = this.buildRequestBody(codCusto[0], codDir[0], codArea[0], codDep, codFunc);
     return this.http.post<any>(url, body, { headers: this.getHeaders() });
   }
 
   // POST para diretores
   postDiretores(codCusto: Array<any>, codDir: Array<any>, codArea: Array<any>, codDep: Array<any>, codFunc: Array<any>): Observable<any> {
     const url = `${this.apiUrl}/get_diretores`;
-    const body = this.buildRequestBody(codCusto, codDir, codArea, codDep, codFunc);
+    const body = this.buildRequestBody(codCusto[0], codDir[0], codArea[0], codDep, codFunc);
     return this.http.post<any>(url, body, { headers: this.getHeaders() });
   }
 
   // POST para áreas
   postAreas(codCusto: Array<any>, codDir: Array<any>, codArea: Array<any>, codDep: Array<any>, codFunc: Array<any>): Observable<any> {
     const url = `${this.apiUrl}/get_areas`;
-    const body = this.buildRequestBody(codCusto, codDir, codArea, codDep, codFunc);
+    const body = this.buildRequestBody(codCusto[0], codDir[0], codArea[0], codDep, codFunc);
     return this.http.post<any>(url, body, { headers: this.getHeaders() });
   }
 
   // POST para departamentos
   postDeptos(codCusto: Array<any>, codDir: Array<any>, codArea: Array<any>, codDep: Array<any>, codFunc: Array<any>): Observable<any> {
     const url = `${this.apiUrl}/get_depto`;
-    const body = this.buildRequestBody(codCusto, codDir, codArea, codDep, codFunc);
+    const body = this.buildRequestBody(codCusto[0], codDir[0], codArea[0], codDep, codFunc);
     return this.http.post<any>(url, body, { headers: this.getHeaders() });
   }
 
   // POST para funções
   postFuncoes(codCusto: Array<any>, codDir: Array<any>, codArea: Array<any>, codDep: Array<any>, codFunc: Array<any>): Observable<any> {
     const url = `${this.apiUrl}/get_funcoes`;
-    const body = this.buildRequestBody(codCusto, codDir, codArea, codDep, codFunc);
+    const body = this.buildRequestBody(codCusto[0], codDir[0], codArea[0], codDep, codFunc);
     return this.http.post<any>(url, body, { headers: this.getHeaders() });
   }
 
   // POST para tabela
   postTable(codCusto: Array<any>, codDir: Array<any>, codArea: Array<any>, codDep: Array<any>, codFunc: Array<any>, dataIni: string | Date): Observable<any> {
     const url = `${this.apiUrl}/get_table`;
-    const body = this.buildRequestBody(codCusto, codDir, codArea, codDep, codFunc, dataIni);
+    const body = this.buildRequestBody(codCusto[0], codDir[0], codArea[0], codDep, codFunc, dataIni);
     return this.http.post<any>(url, body, { headers: this.getHeaders() });
   }
 }
